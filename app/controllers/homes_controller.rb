09400444fcdac216f9743b98651039fe1bfaaa6b
orange41 <figiting-nemo@hotmail.co.jp>
2024-11-23 16:05:33 +0000

    admin画面ユーザーまで一応完了

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,19 +1,24 @@
+# app/controllers/homes_controller.rb
 class HomesController < ApplicationController
   def index
     # トップページの表示
   end
 
   def user_login
-    # 必要なロジックをここに追加
+    # ユーザーログインページの表示
     render 'user_login'
-     # これにより、user_login.html.erb テンプレートをレンダリング
   end
 
   def admin_login
     # 管理者ログインページの表示
+    render 'admin_login'
   end
 
   def user_dashboard
     # ユーザーダッシュボードの表示
+  end
+
+  def admin_dashboard
+    # 管理者ダッシュボードの表示
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'user_login', to: 'homes#user_login'
   get 'admin_login', to: 'homes#admin_login'
   get 'user_dashboard', to: 'homes#user_dashboard'
+  get 'admin_dashboard', to: 'homes#admin_dashboard'
 end
-
