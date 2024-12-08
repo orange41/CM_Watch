584be04a2cd7bd901ab0629ce39a937a1c1de647
orange41 <figiting-nemo@hotmail.co.jp>
2024-12-08 13:47:30 +0000

    デプロイ1stフェーズ完了

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -1,4 +1,8 @@
 # app/controllers/admins/sessions_controller.rb
 class Admins::SessionsController < Devise::SessionsController
   # Adminのログイン処理
+  def create
+    Rails.logger.info "Admin ログイン試行: #{params[:admin][:employee_number]}"
+    super
+  end
 end
