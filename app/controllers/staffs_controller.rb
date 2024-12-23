26a29f1570069b5f6f5bc154222f1f1fc39e1276
EC2 Default User <ec2-user@ip-172-31-14-162.ap-northeast-1.compute.internal>
2024-12-23 10:36:48 +0000

    デザイン中

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -1,21 +1,19 @@
 class StaffsController < ApplicationController
   before_action :authenticate_staff!, except: [:index]
   before_action :authenticate_admin!, only: [:index] # 管理者がアクセスできるようにする
+  before_action :set_staff, only: [:show, :edit, :update, :destroy]
 
   def index
     @staffs = Staff.all
   end
 
   def show
-    @staff = Staff.find(params[:id])
   end
 
   def edit
-    @staff = current_staff
   end
 
   def update
-    @staff = current_staff
     if @staff.update(staff_params)
       bypass_sign_in(@staff) # パスワード更新後にセッションを再設定
       redirect_to @staff, notice: 'プロフィールが更新されました。'
@@ -25,6 +23,10 @@ class StaffsController < ApplicationController
   end
 
   private
+
+  def set_staff
+    @staff = current_staff
+  end
 
   def staff_params
     params.require(:staff).permit(:employee_number, :name, :password, :password_confirmation).tap do |staff_params|
