f8a099ac36a7772e8e4753c4f9b2a6a4e273c779
EC2 Default User <ec2-user@ip-172-31-14-162.ap-northeast-1.compute.internal>
2025-01-04 17:34:50 +0000

    完成

--- db/migrate/20241123132537_create_incidents.rb
+++ db/migrate/20241123132537_create_incidents.rb
@@ -3,12 +3,12 @@ class CreateIncidents < ActiveRecord::Migration[6.1]
     create_table :incidents do |t|
       t.string :title
       t.text :description
-      t.datetime :occurred_at # 発生日時フィールド
-      t.integer :staff_id # スタッフIDフィールド
+      t.datetime :occurred_at # 発生日時
+      t.integer :staff_id # スタッフID
 
       t.timestamps
     end
 
-    add_index :incidents, :staff_id # スタッフIDにインデックス
+    add_index :incidents, :staff_id # スタッフIDインデ
   end
 end
